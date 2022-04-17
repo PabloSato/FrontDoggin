790817e3b05a69ee8d59619bc992cfa9bcb382cf
Pablo Fernández Sato <pablofersato@gmail.com>
2022-04-17 09:54:41 +0200

    añadido funcion createEvento

---
+++ src/composables/Evento/createEvento.js
@@ -0,0 +1,31 @@
+import { ref } from 'vue';
+
+const createEvento = (nuevoEvento, idAdiestrador) => {
+  const error = ref(null);
+  const evento = ref(null);
+  const ev = {
+    idAdiestrador: idAdiestrador,
+    nombre: nuevoEvento.nombre,
+    fecha: nuevoEvento.fecha,
+    maxAforo: nuevoEvento.maxAforo,
+  };
+
+  const insertEvento = async () => {
+    try {
+      let data = await fetch('http://localhost:3000/eventos', {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(ev),
+      });
+      if (!data.ok) throw Error('error al crear el evento');
+      evento.value = await data.json();
+    } catch (err) {
+      error.value = err.message;
+      console.log(error.value);
+    }
+  };
+
+  return { evento, error, insertEvento };
+};
+
+export default createEvento;

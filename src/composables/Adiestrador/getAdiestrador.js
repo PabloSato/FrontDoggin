597430babd9ec6cc64e4cd574b2827c5ef08e8e5
Pablo Fernández Sato <pablofersato@gmail.com>
2022-04-17 18:16:46 +0200

    atrapado en edit, creo que hay que usar lodash pero no sé como

--- src/composables/Adiestrador/getAdiestrador.js
+++ src/composables/Adiestrador/getAdiestrador.js
@@ -7,14 +7,13 @@ const getAdiestrador = id => {
   const load = async () => {
     try {
       let data = await fetch('http://localhost:3000/adiestradores/' + id);
-      if (!data.ok) throw Error('error al coger adiestrador');
+      if (!data.ok) throw new Error('Error al obtener adiestrador');
       adiestrador.value = await data.json();
     } catch (err) {
       error.value = err.message;
       console.log(error.value);
     }
   };
-
   return { adiestrador, error, load };
 };
 

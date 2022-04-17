dfbb395738400ffe7c16811ceee02d444204ed3d
hannah-bannanah <hannah-bannanah@users.noreply.github.com>
2022-04-17 12:36:03 +0200

    añadido vistas de adiestradores y eventos con filtrado en eventos

--- src/components/Adiestrador.vue
+++ src/components/Adiestrador.vue
@@ -1,14 +1,28 @@
 <template>
-  <h3>{{ adiestrador.nombre }}</h3>
+  <div class="card">
+    <h3>{{ adiestrador.nombre }}</h3>
+    <p>{{ adiestrador.bio }}</p>
+    <p>
+      Rating: <span v-if="adiestrador.rating">{{ adiestrador.rating }}</span
+      ><span v-else>No disponible</span>
+    </p>
+    <button @click="verEventos(adiestrador._id)">Ver eventos</button>
+  </div>
 </template>
 
 <script>
+import { useRouter } from 'vue-router';
 export default {
   props: ['adiestrador'],
-  setup () {}
+  setup() {
+    const router = useRouter();
+    const verEventos = id => {
+      router.push({ path: `/adiestradores/${id}/eventos` });
+    };
+
+    return { verEventos };
+  },
 };
 </script>
 
-<style>
-
-</style>
+<style></style>

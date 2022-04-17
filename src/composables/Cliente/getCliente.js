d1f9a2f68e8851d91a6ebc6bed28d6d2729fc297
Pablo Fernández Sato <pablofersato@gmail.com>
2022-04-17 09:22:30 +0200

    correccion getCliente

--- src/composables/Cliente/getCliente.js
+++ src/composables/Cliente/getCliente.js
@@ -1,36 +1,20 @@
 import { ref } from 'vue';
 
 const getCliente = id => {
-  const clienteID = ref(null);
-  const user = ref(null);
   const error = ref(null);
   const cliente = ref(null);
 
   const load = async () => {
     try {
-      let data = await fetch('http://localhost:3001/clientes/' + id);
-      if (!data.ok) {
-        throw new Error('errorrrrr');
-      } else {
-        clienteID.value = await data.json();
-        let idUser = clienteID.value.userId;
-        let data2 = await fetch('http://localhost:3000/users/' + idUser);
-        if (!data.ok) {
-          throw new Error('error x2');
-        } else {
-          user.value = await data2.json();
-          let cli = {
-            nombre: clienteID.value.nombre,
-            email: user.value.email,
-          };
-          cliente.value = cli;
-        }
-      }
+      let data = await fetch('http://localhost:3000/clientes/' + id);
+      if (!data.ok) throw Error('error al coger al cliente');
+      cliente.value = await data.json();
     } catch (err) {
       error.value = err.message;
       console.log(error.value);
     }
   };
+
   return { cliente, error, load };
 };
 

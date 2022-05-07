7221b231427aae7a539a179c779c48217c706d05
Pablo Fernández Sato <pablofersato@gmail.com>
2022-05-07 20:22:16 +0200

    correccion formulario mensaje

--- src/components/Formularios/FormMail.vue
+++ src/components/Formularios/FormMail.vue
@@ -1,28 +1,42 @@
 <template>
   <div class="contForm">
     <div class="subContForm">
-      <h2>Envio de anuncio</h2>
-      <p>¡¡Manda un anuncio a tus clientes!!</p>
-      <img src="https://cdn.pixabay.com/photo/2018/01/31/05/32/post-3120315_960_720.jpg" alt="image"/>
+      <h2>{{ titulo }}</h2>
+      <p>{{ subTitulo }}</p>
+      <img
+        src="https://cdn.pixabay.com/photo/2018/01/31/05/32/post-3120315_960_720.jpg"
+        alt="image"
+      />
       <div class="container-text">
         <p v-if="errorEnvio">{{ errorEnvio }}</p>
         <div v-if="errorValida.length > 0">
           <p v-for="mensaje in errorValida" :key="mensaje">{{ mensaje }}</p>
         </div>
         <form @submit.prevent="procesaFormu">
-          <input type="text" v-model="mail.asunto" placeholder="Título del anuncio" required />  
-          <textarea v-model="mail.mensaje" id="mensaje"  rows="5" placeholder="Texto del anuncio" required></textarea>
+          <input
+            type="text"
+            v-model="mail.asunto"
+            placeholder="Asunto del mensaje"
+            required
+          />
+          <textarea
+            v-model="mail.mensaje"
+            id="mensaje"
+            rows="5"
+            placeholder="Texto del mensaje"
+            required
+          ></textarea>
           <button type="submit" value="enviar">Enviar</button>
           <button class="alter" type="reset" value="borrar">Borrar</button>
         </form>
-      </div>    
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['mail', 'errorEnvio', 'errorValida'],
+  props: ['mail', 'errorEnvio', 'errorValida', 'titulo', 'subTitulo'],
   emits: ['formProce'],
   setup(props, context) {
     //Variables
--- src/views/Adiestrador/AdiMiPerfil.vue
+++ src/views/Adiestrador/AdiMiPerfil.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="contForm">
+  <div class="contForm" v-if="adiestrador">
     <div class="subContForm">
       <h2>{{ adiestrador.nombre }}</h2>
       <img :src="adiestrador.imageUrl" :alt="adiestrador.nombre" />
@@ -16,14 +16,6 @@
       </div>
     </div>
   </div>
-
-
-
-
-
-
-
-
 
   <!-- <div v-if="adiestrador" class="perfil">
     <h2>Nombre: {{ adiestrador.nombre }}</h2>
@@ -80,34 +72,32 @@ export default {
   padding: 0;
   box-sizing: border-box;
 
-h2{
-  font-size: 2.3rem;
-  color: #dfe6ee;
-  font-weight: 500;
-  margin-bottom: 15px;
-}
-p{
-  font-size: 1rem;
-  color: #dfe6ee;
-  font-weight: 100;
-  margin-bottom: 5px;
+  h2 {
+    font-size: 2.3rem;
+    color: #dfe6ee;
+    font-weight: 500;
+    margin-bottom: 15px;
+  }
+  p {
+    font-size: 1rem;
+    color: #dfe6ee;
+    font-weight: 100;
+    margin-bottom: 5px;
+  }
 
+  .alertaForm {
+    font-size: 1rem;
+    color: #ff5733;
+    font-weight: 500;
+    margin-bottom: 5px;
+  }
 }
-
-.alertaForm{
-  font-size: 1rem;
-  color: #FF5733;
-  font-weight: 500;
-  margin-bottom: 5px;
-}
-
-}.contForm{
+.contForm {
   width: 100%;
   display: flex;
   justify-content: center;
-
 }
-.subContForm{
+.subContForm {
   background-color: #5f4bb6;
   width: 100%;
   max-width: 1024px;
@@ -117,7 +107,7 @@ p{
   line-height: 1.5;
   padding: 30px;
   box-shadow: 0 20px 30px rgba(0, 0, 0, 0.185);
-  
+
   img {
     width: 100%;
     max-height: 200px;
@@ -137,14 +127,14 @@ p{
 
   input,
   textarea {
-    border: 2px solid #DADDEC;
+    border: 2px solid #daddec;
     margin: 5px 0 10px;
     font-size: 1rem;
     color: #656880;
   }
 
   button {
-    background:#9ab7d7;
+    background: #9ab7d7;
     display: block;
     color: #5f4bb6;
     font-size: 1rem;
@@ -154,50 +144,46 @@ p{
   button:hover {
     box-shadow: 0 5px 20px #89caff94;
     transition: box-shadow 0.3s ease-in-out;
-
   }
 }
-.botonesRow{
+.botonesRow {
   display: flex;
   flex-direction: column;
 }
 .alter {
   background: #170f11 !important;
-} 
+}
 .alter:hover {
-    box-shadow: 0 5px 20px #170f11 !important;
-    transition: box-shadow 0.3s ease-in-out !important;
-  }
+  box-shadow: 0 5px 20px #170f11 !important;
+  transition: box-shadow 0.3s ease-in-out !important;
+}
 
-.container-text{
+.container-text {
   text-align: center;
   padding: 10px 10px 10px 10px;
   min-height: 100%;
 }
 
-
 @media screen and (min-width: 750px) {
-.subContForm{
-  display: flex;
-  width: 90%;
-  max-width: 1024px;
+  .subContForm {
+    display: flex;
+    width: 90%;
+    max-width: 1024px;
 
     img {
-    width: 660px;
-      
-    button {
-      width: 10rem;
+      width: 660px;
 
+      button {
+        width: 10rem;
+      }
+    }
   }
+  .botonesRow {
+    display: flex;
+    flex-direction: row;
   }
-}
-.botonesRow{
-  display: flex;
-  flex-direction: row;
-}
-.container-text{
-width:490px; 
-}
-
+  .container-text {
+    width: 490px;
+  }
 }
 </style>

cf0d0572883ec3449e20d5ddbec1e22a48e0ce10
david <david.fernandez.peralta@gmail.com>
2022-05-07 21:15:59 +0200

    vista adiestrador

--- src/views/Adiestrador/DetalleAdiestrador.vue
+++ src/views/Adiestrador/DetalleAdiestrador.vue
@@ -1,32 +1,31 @@
 <template>
   <div class="contForm">
     <div v-if="adiestrador" class="subContForm">
-        <h2>{{ adiestrador.nombre }}</h2>
-        <img :src="adiestrador.imageUrl" :alt="adiestrador.nombre" />
-        <h2>Bio</h2>
-        <p>{{ adiestrador.bio }}</p>
-        <h2>Rating</h2>
-        <p>{{ adiestrador.rating }}</p>
-        <div v-if="isLogin">
-          <FormValoracion
-            :adiestrador="adiestrador"
-            :valoracion="valoracion"
-            :errorValora="errorValora"
-            :okValora="okValora"
-            @proValorar="valoraFuncion(valoracion)"
-          />
-          <router-link
-            :to="{
-              name: 'contacto',
-              params: { id: adiestrador._id, nombre: adiestrador.nombre },
-            }"
-            >Contacto</router-link
-          >
-          <button @click="verEventos(adiestrador._id)">ver eventos</button>
+      <h2>{{ adiestrador.nombre }}</h2>
+      <img :src="adiestrador.imageUrl" :alt="adiestrador.nombre" />
+      <h2>Bio</h2>
+      <p>{{ adiestrador.bio }}</p>
+      <h2>Rating</h2>
+      <p>{{ adiestrador.rating }}/5 puntos</p>
+      <div v-if="isLogin">
+        <FormValoracion
+          :adiestrador="adiestrador"
+          :valoracion="valoracion"
+          :errorValora="errorValora"
+          :okValora="okValora"
+          @proValorar="valoraFuncion(valoracion)"
+        />
+        <!-- <router-link
+          :to="{
+            name: 'contacto',
+            params: { id: adiestrador._id, nombre: adiestrador.nombre },
+          }"
+          >Contacto</router-link> -->
+        <div class="botonesRow">
+          <button @click="contactos()">Contactos</button>
+          <button @click="verEventos(adiestrador._id)">Eventos</button>
         </div>
-      
-
-
+      </div>
     </div>
   </div>
 </template>
@@ -55,6 +54,11 @@ export default {
     const { adiestrador, error, loadAdiestrador } = getAdiestraod(props.id);
     let token = '';
     //Funciones
+
+    const contactos = () =>  {
+    router.push({ name: adiestrador.nombre, id: adiestrador._id});
+    };
+
     loadAdiestrador();
     if (
       (localStorage.getItem('token') !== null ||
@@ -67,7 +71,6 @@ export default {
     const verEventos = id => {
       router.push({ path: `/adiestradores/${id}/eventos` });
     };
-
     //Funcion Valorar
     const valoraFuncion = async valoracion => {
       const { rating, error, insertValoracion } = valorarAdiestrador(
@@ -85,6 +88,7 @@ export default {
     };
     //Return
     return {
+      verEventos,
       adiestrador,
       isLogin,
       verEventos,
@@ -98,7 +102,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
 * {
   margin: 0;
   padding: 0;
@@ -109,34 +112,32 @@ export default {
     width: 100vw;
     min-height: 90vh;
   }
-h2{
-  font-size: 2.3rem;
-  color: #dfe6ee;
-  font-weight: 500;
-  margin-bottom: 15px;
-}
-p{
-  font-size: 1rem;
-  color: #dfe6ee;
-  font-weight: 100;
-  margin-bottom: 5px;
+  h2 {
+    font-size: 2.3rem;
+    color: #dfe6ee;
+    font-weight: 500;
+    margin-bottom: 15px;
+  }
+  p {
+    font-size: 1rem;
+    color: #dfe6ee;
+    font-weight: 100;
+    margin-bottom: 5px;
+  }
 
+  .alertaForm {
+    font-size: 1rem;
+    color: #ff5733;
+    font-weight: 500;
+    margin-bottom: 5px;
+  }
 }
-
-.alertaForm{
-  font-size: 1rem;
-  color: #FF5733;
-  font-weight: 500;
-  margin-bottom: 5px;
-}
-
-}.contForm{
+.contForm {
   width: 100%;
   display: flex;
   justify-content: center;
-
 }
-.subContForm{
+.subContForm {
   background-color: #5f4bb6;
   width: 100%;
   max-width: 1024px;
@@ -146,7 +147,7 @@ p{
   line-height: 1.5;
   padding: 30px;
   box-shadow: 0 20px 30px rgba(0, 0, 0, 0.185);
-  
+
   img {
     width: 100%;
     max-height: 200px;
@@ -166,14 +167,14 @@ p{
 
   input,
   textarea {
-    border: 2px solid #DADDEC;
+    border: 2px solid #daddec;
     margin: 5px 0 10px;
     font-size: 1rem;
     color: #656880;
   }
 
   button {
-    background:#9ab7d7;
+    background: #9ab7d7;
     display: block;
     color: #5f4bb6;
     font-size: 1rem;
@@ -183,50 +184,47 @@ p{
   button:hover {
     box-shadow: 0 5px 20px #89caff94;
     transition: box-shadow 0.3s ease-in-out;
-
   }
 }
-.botonesRow{
+.botonesRow {
   display: flex;
   flex-direction: column;
+  width: 20rem;
 }
 .alter {
   background: #170f11 !important;
-} 
+}
 .alter:hover {
-    box-shadow: 0 5px 20px #170f11 !important;
-    transition: box-shadow 0.3s ease-in-out !important;
-  }
+  box-shadow: 0 5px 20px #170f11 !important;
+  transition: box-shadow 0.3s ease-in-out !important;
+}
 
-.container-text{
+.container-text {
   text-align: center;
   padding: 10px 10px 10px 10px;
   min-height: 100%;
 }
 
-
 @media screen and (min-width: 750px) {
-.subContForm{
-  display: flex;
-  width: 90%;
-  max-width: 1024px;
+  .subContForm {
+    display: flex;
+    width: 90%;
+    max-width: 1024px;
 
     img {
-    width: 660px;
-      
-    button {
-      width: 10rem;
+      width: 660px;
 
+      button {
+        width: 10rem;
+      }
+    }
   }
+  .botonesRow {
+    display: flex;
+    flex-direction: row;
   }
-}
-.botonesRow{
-  display: flex;
-  flex-direction: row;
-}
-.container-text{
-width:490px; 
-}
-
+  .container-text {
+    width: 490px;
+  }
 }
 </style>

2153b7363e12f3616a6efdf8b4cc24e01a73f0d5
hannah-bannanah <hannah-bannanah@users.noreply.github.com>
2022-05-08 10:47:46 +0200

    mejorado busqueda adiestrador

--- src/components/Adiestrador/ListaAdiestradores.vue
+++ src/components/Adiestrador/ListaAdiestradores.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="adiestradores">
     <input type="text" v-model="search" placeholder="Buscar" />
+    <Multiselect
+      v-model="filterTags"
+      mode="tags"
+      placeholder="Selecciona filtros"
+      :options="TAGS"
+      :search="true"
+    />
     <adiestrador
       v-for="adiestrador in adiestradoresVisibles"
       :key="adiestrador._id"
@@ -15,19 +22,46 @@ import Adiestrador from './Adiestrador.vue';
 //Composables
 import getAdiestradores from '../../composables/Adiestrador/getAdiestradores';
 import { computed, ref } from '@vue/runtime-core';
+import Multiselect from '@vueform/multiselect';
+
 export default {
-  components: { Adiestrador },
+  components: { Adiestrador, Multiselect },
   setup() {
     const search = ref(null);
     search.value = '';
+
+    const TAGS = ref([]);
+    TAGS.value = ['Agility', 'Cachorros', 'Razas grandes', 'Pastoreo'];
+
+    const filterTags = ref([]);
+
     const { adiestradores, load } = getAdiestradores();
     load();
     const adiestradoresVisibles = computed(() => {
-      return adiestradores.value.filter(a =>
+      let adiestradoresBuscados = adiestradores.value.filter(a =>
         a.nombre.toLowerCase().match(search.value.toLowerCase())
       );
+      if (filterTags.value.length) {
+        adiestradoresBuscados = adiestradoresBuscados.filter(a => {
+          const resultado = a.tags.find(
+            aT => !!filterTags.value.find(t => t === aT)
+          );
+          return !!resultado;
+        });
+      }
+      return adiestradoresBuscados;
     });
-    return { adiestradoresVisibles, search };
+
+    const filterAdiestradores = () => {
+      alert('ya selected!');
+    };
+    return {
+      adiestradoresVisibles,
+      search,
+      TAGS,
+      filterTags,
+      filterAdiestradores,
+    };
   },
 };
 </script>
